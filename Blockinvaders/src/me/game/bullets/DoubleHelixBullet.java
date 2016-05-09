e0b757e56a9b6440bed8565918aa913808c1b283
Totenfluch <ziegler.christian@kabelmail.de>
2016-05-09 23:34:57 +0200

    better Helix effect

--- Blockinvaders/src/me/game/bullets/DoubleHelixBullet.java
+++ Blockinvaders/src/me/game/bullets/DoubleHelixBullet.java
@@ -9,21 +9,30 @@ import me.game.weapons.MonsterWeapon;
 import me.game.weapons.PlayerWeapon;
 
 public class DoubleHelixBullet extends MonsterBullet{
-	
+
 	boolean type;
 	double wave=0;
-	
-	
+	DoubleHelixBullet Partner;
+
+
 	public DoubleHelixBullet(MonsterWeapon waffe, double xPos, double yPos, double height, double width, int speed, int damage,
 			double angle, boolean type) {
 		super(waffe, xPos, yPos, height, width, speed, damage, angle);
 		this.type = type;
 	}
 
+	public DoubleHelixBullet getPartner(){
+		return Partner;
+	}
+
+	public void setPartner(DoubleHelixBullet Partner){
+		this.Partner = Partner;
+	}
+
 	public void refresh(){
 		if(yPos <= 0 || yPos >= Frame.GAME_LENGTH || xPos <= 0 || xPos >= Frame.GAME_WIDTH)
 			waffe.getKugeln().remove(this);
-		
+
 		for (int x = 0; x < waffe.getOwner().game.Players.length; x++) {
 			Player p = waffe.getOwner().game.Players[x];
 			if (checkHit(p.getX(), p.getY(), p.getWidth(), p.getHeight())) {
@@ -38,9 +47,9 @@ public class DoubleHelixBullet extends MonsterBullet{
 				}
 			}
 		}
-		
+
 		if(type){
-			xPos = xPos+(Math.sin(wave)*3);
+			xPos = xPos+(Math.sin(wave+Math.PI)*3);
 		}else{
 			xPos = xPos+(Math.cos(wave)*3);
 		}
@@ -55,5 +64,13 @@ public class DoubleHelixBullet extends MonsterBullet{
 		else
 			gc.setFill(Color.INDIGO);
 		gc.fillRect(xPos, yPos, width, height);
+
+		gc.setFill(Color.GREEN);
+		if(Partner != null)
+			if(Partner.getxPos() > xPos)
+				gc.fillRect(xPos+width, yPos+height/2, Partner.getxPos()-xPos-width, Partner.getyPos()-yPos+height/4);
+			else
+				gc.fillRect(xPos+width, yPos+height/2, xPos-Partner.getxPos()-width, Partner.getyPos()-yPos+height/4);
+
 	}
 }
--- Blockinvaders/src/me/game/weapons/MonsterDoubleHelixWeapon.java
+++ Blockinvaders/src/me/game/weapons/MonsterDoubleHelixWeapon.java
@@ -13,7 +13,10 @@ public class MonsterDoubleHelixWeapon extends MonsterWeapon{
 
 	@Override
 	public void shoot(double monsterX, double monsterY) {
-		kugeln.add(new DoubleHelixBullet(this, monsterX, monsterY, bulletH+2, bulletW+2, bulletSpeed, damage, 0, true));
-		kugeln.add(new DoubleHelixBullet(this, monsterX, monsterY, bulletH+2, bulletW+2, bulletSpeed, damage, 0, false));
+		DoubleHelixBullet main = new DoubleHelixBullet(this, monsterX, monsterY, bulletH+2, bulletW+2, bulletSpeed, damage, 0, true);
+		kugeln.add(main);
+		DoubleHelixBullet sec = new DoubleHelixBullet(this, monsterX, monsterY, bulletH+2, bulletW+2, bulletSpeed, damage, 0, false);
+		kugeln.add(sec);
+		main.setPartner(sec);
 	}
 }
